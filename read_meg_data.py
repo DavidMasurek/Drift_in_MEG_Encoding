7070d26c0dbaa906f8be718a23faefb9023b45bb
DavidinAwe <david.masurek.dm@gmail.com>
2024-04-14 17:02:02 +0200

    Load AVS repo functionality correctly

--- read_meg_data.py
+++ read_meg_data.py
@@ -1,14 +1,30 @@
 import json
 import sys
+import importlib.util
 
-# setting path to import functionalities from AVS-machine-room
-sys.path.append('../AVS-machine-room-copy')
+# Add AVS directory to sys.path to allow import
+avs_project_root_path = "/share/klab/camme/camme/dmasurek/AVS-machine-room-copy/"
+sys.path.insert(0, avs_project_root_path)
 
-from AVS-machine-room-copy.avs_machine_room.dataloader.load_population_codes.load_h5 import load_pop_code_dynamics
+# Load the required functions
+from avs_machine_room.dataloader.load_population_codes.load_h5 import load_pop_code_dynamics
 
 # Read crop_dict from json
-crop_dict_file = open("crop_dict.json")
+crop_dict_file = open("data_files/crop_dict.json")
 crop_dict_string = crop_dict_file.read()
 crop_dict = json.loads(crop_dict_string)
 
-#print(crop_dict["sessions"]["1"]["trials"]["1"]["timepoints"]["0.2829999999990105"])
+# Load and inspect MEG data
+path_to_meg_data = "/share/klab/datasets/avs/population_codes/as02/sensor/filter_0.2_200"
+meg_data_file = "as02a_population_codes_fixation_500hz_masked_False.h5"
+
+pop_code_dynamics = load_pop_code_dynamics(subject=["2"], 
+                                        session=1,  
+                                        input_dir=path_to_meg_data, 
+                                        event_type="fixation", 
+                                        s_freq=500, 
+                                        masked=False, 
+                                        take_norm=True
+                    )
+
+print(f"pop_code_dynamics.keys(): {pop_code_dynamics.keys()}")
